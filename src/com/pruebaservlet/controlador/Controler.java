8cb6ce943b9d63356b8a2258a404a9cc4b6ac0a1
Erick Pacheco <erickpacheco@Dragonblack.local>
2019-04-06 23:57:07 -0600

    Test Servlet in java web

---
+++ src/com/pruebaservlet/controlador/Controler.java
@@ -0,0 +1,47 @@
+package com.pruebaservlet.controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Controler
+ */
+@WebServlet("/Controler")
+public class Controler extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controler() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		String termino = request.getParameter("txtBusqueda");
+		response.sendRedirect("http://google.com/search?q="+termino);
+		//response.sendRedirect("redirigir.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		
+		String nombre = request.getParameter("txtNombre");
+		response.getWriter().append("Bienvenido Usuario "+nombre);
+	}
+
+}
